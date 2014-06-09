7450621e6388f48b8178c75e60bef6312f8670c8
m4gik <m4giksoftware@gmail.com>
2014-06-09 14:54:49 +0200

    Add class for network state

---
+++ bitcoin-excavator/excavator/src/main/java/com/bitcoin/core/network/NetworkState.java
@@ -0,0 +1,17 @@
+/**
+ * Project BitcoinExcavator.
+ * Copyright Michał Szczygieł & Aleksander Śmierciak
+ * Created at June 9, 2014.
+ */
+
+package com.bitcoin.core.network;
+
+/**
+ * TODO COMMENTS MISSING!
+ * 
+ * @author m4gik <michal.szczygiel@wp.pl>
+ * 
+ */
+public class NetworkState {
+
+}
