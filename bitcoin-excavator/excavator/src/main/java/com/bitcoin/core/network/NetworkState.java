9c2dac6b53fa737d33046aa438c1fd109605d4b4
m4gik <m4giksoftware@gmail.com>
2014-06-19 16:42:32 +0200

    Change class to abstract class

--- bitcoin-excavator/excavator/src/main/java/com/bitcoin/core/network/NetworkState.java
+++ bitcoin-excavator/excavator/src/main/java/com/bitcoin/core/network/NetworkState.java
@@ -14,7 +14,7 @@ import java.util.concurrent.atomic.AtomicLong;
  *
  * @author m4gik <michal.szczygiel@wp.pl>
  */
-public class NetworkState {
+public abstract class NetworkState {
 
     private Byte hostChain;
 
