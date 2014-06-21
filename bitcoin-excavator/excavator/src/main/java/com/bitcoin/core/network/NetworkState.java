7db7d76a2a3888677213eba8e30dabb19bb31a6b
m4gik <m4giksoftware@gmail.com>
2014-06-21 19:49:23 +0200

    Add comments

--- bitcoin-excavator/excavator/src/main/java/com/bitcoin/core/network/NetworkState.java
+++ bitcoin-excavator/excavator/src/main/java/com/bitcoin/core/network/NetworkState.java
@@ -6,53 +6,54 @@
 
 package com.bitcoin.core.network;
 
-import com.bitcoin.core.Excavator;
-import com.bitcoin.core.device.ExecutionState;
-
 import java.net.URL;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.bitcoin.core.Excavator;
+import com.bitcoin.core.device.ExecutionState;
+
 /**
- * This class is responsible for provide gathering information about network state.
- *
+ * This class is responsible for provide gathering information about network
+ * state.
+ * 
  * @author m4gik <michal.szczygiel@wp.pl>
  */
 public abstract class NetworkState {
-
-    public static final byte PROTOCOL_JSONRPC = 0;
-
-    public static final byte PROTOCOL_STRATUM = 1;
 
     public static final byte CHAIN_BITCOIN = 0;
 
     public static final byte CHAIN_LITECOIN = 1;
 
+    public static final byte PROTOCOL_JSONRPC = 0;
+
+    public static final byte PROTOCOL_STRATUM = 1;
+
     public static final String STRATUM = "stratum";
 
     private Excavator excavator;
+
+    protected LinkedBlockingDeque<ExecutionState> getQueue;
 
     private Byte hostChain;
 
     private Byte hostProtocol;
 
-    private Long workLifetime;
-
-    private Boolean rollNTime;
-
-    private URL queryUrl;
-
-    private String user;
+    private NetworkState networkStateNext;
 
     private String password;
 
+    private URL queryUrl;
+
     protected AtomicLong refreshTimestamp;
 
-    private NetworkState networkStateNext;
-
-    protected LinkedBlockingDeque<ExecutionState> getQueue;
+    private Boolean rollNTime;
 
     protected LinkedBlockingDeque<WorkState> sendQueue;
+
+    private String user;
+
+    private Long workLifetime;
 
     /**
      * Constructor for {@link com.bitcoin.core.network.NetworkState} class.
@@ -65,16 +66,20 @@ public abstract class NetworkState {
 
     /**
      * Constructor for {@link com.bitcoin.core.network.NetworkState} class.
-     *
-     * @param excavator The instance of excavator.
-     * @param hostChain The chain of hosts.
-     * @param queryUrl  The url query.
-     * @param user      The username for mine
-     * @param password  The password
+     * 
+     * @param excavator
+     *            The instance of excavator.
+     * @param hostChain
+     *            The chain of hosts.
+     * @param queryUrl
+     *            The url query.
+     * @param user
+     *            The username for mine
+     * @param password
+     *            The password
      */
     public NetworkState(Excavator excavator, URL queryUrl, Byte hostChain,
-            String user,
-            String password) {
+            String user, String password) {
         setExcavator(excavator);
         setQueryUrl(queryUrl);
         setHostChain(hostChain);
@@ -93,101 +98,173 @@ public abstract class NetworkState {
     /**
      * This method adds {@link com.bitcoin.core.device.ExecutionState} instance
      * to {@link java.util.concurrent.LinkedBlockingDeque}.
-     *
-     * @param executionState The executionState to add.
+     * 
+     * @param executionState
+     *            The executionState to add.
      */
     public void addGetQueue(ExecutionState executionState) {
         this.getQueue.add(executionState);
     }
 
     /**
-     * This method adds {} ins@link WorkState} instance
-     * to {@link java.util.concurrent.LinkedBlockingDeque}.
-     *
-     * @param workState Th workState to add.
+     * This method adds {} ins@link WorkState} instance to
+     * {@link java.util.concurrent.LinkedBlockingDeque}.
+     * 
+     * @param workState
+     *            Th workState to add.
      */
     public void addSendQueue(WorkState workState) {
         this.sendQueue.add(workState);
     }
 
+    /**
+     * @return the excavator
+     */
     public Excavator getExcavator() {
         return excavator;
     }
 
-    public void setExcavator(Excavator excavator) {
-        this.excavator = excavator;
-    }
-
+    /**
+     * @return the hostChain
+     */
     public Byte getHostChain() {
         return hostChain;
     }
 
-    public void setHostChain(Byte hostChain) {
-        this.hostChain = hostChain;
-    }
-
+    /**
+     * @return the hostProtocol
+     */
     public Byte getHostProtocol() {
         return hostProtocol;
     }
 
-    public void setHostProtocol(Byte hostProtocol) {
-        this.hostProtocol = hostProtocol;
-    }
-
-    public Long getWorkLifetime() {
-        return workLifetime;
-    }
-
-    public void setWorkLifetime(Long workLifetime) {
-        this.workLifetime = workLifetime;
-    }
-
-    public Boolean getRollNTime() {
-        return rollNTime;
-    }
-
-    public void setRollNTime(Boolean rollNTime) {
-        this.rollNTime = rollNTime;
-    }
-
-    public URL getQueryUrl() {
-        return queryUrl;
-    }
-
-    public void setQueryUrl(URL queryUrl) {
-        this.queryUrl = queryUrl;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getRefreshTimestamp() {
-        return refreshTimestamp.get();
-    }
-
-    public void setRefreshTimestamp(AtomicLong refreshTimestamp) {
-        this.refreshTimestamp = refreshTimestamp;
-    }
-
+    /**
+     * @return the networkStateNext
+     */
     public NetworkState getNetworkStateNext() {
         return networkStateNext;
     }
 
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return the queryUrl
+     */
+    public URL getQueryUrl() {
+        return queryUrl;
+    }
+
+    /**
+     * @return the refreshTimestamp
+     */
+    public Long getRefreshTimestamp() {
+        return refreshTimestamp.get();
+    }
+
+    /**
+     * @return the rollNTime
+     */
+    public Boolean getRollNTime() {
+        return rollNTime;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @return the workLifetime
+     */
+    public Long getWorkLifetime() {
+        return workLifetime;
+    }
+
+    /**
+     * @param excavator
+     *            the excavator to set
+     */
+    public void setExcavator(Excavator excavator) {
+        this.excavator = excavator;
+    }
+
+    /**
+     * @param hostChain
+     *            the hostChain to set
+     */
+    public void setHostChain(Byte hostChain) {
+        this.hostChain = hostChain;
+    }
+
+    /**
+     * @param hostProtocol
+     *            the hostProtocol to set
+     */
+    public void setHostProtocol(Byte hostProtocol) {
+        this.hostProtocol = hostProtocol;
+    }
+
+    /**
+     * @param networkStateNext
+     *            the networkStateNext to set
+     */
     public void setNetworkStateNext(NetworkState networkStateNext) {
         this.networkStateNext = networkStateNext;
+    }
+
+    /**
+     * @param password
+     *            the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @param queryUrl
+     *            the queryUrl to set
+     */
+    public void setQueryUrl(URL queryUrl) {
+        this.queryUrl = queryUrl;
+    }
+
+    /**
+     * @param refreshTimestamp
+     *            the refreshTimestamp to set
+     */
+    public void setRefreshTimestamp(AtomicLong refreshTimestamp) {
+        this.refreshTimestamp = refreshTimestamp;
+    }
+
+    /**
+     * @param rollNTime
+     *            the rollNTime to set
+     */
+    public void setRollNTime(Boolean rollNTime) {
+        this.rollNTime = rollNTime;
+    }
+
+    /**
+     * @param user
+     *            the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @param workLifetime
+     *            the workLifetime to set
+     */
+    public void setWorkLifetime(Long workLifetime) {
+        this.workLifetime = workLifetime;
     }
 
 }
