a6d156a5b4ba6edf8000d1785845bcc31c44bbc3
m4gik <michal.szczygiel@wp.pl>
2014-09-06 14:35:13 +0200

    Fix test for ObjectJsonSerializationTests

--- bitcoin-excavator/excavator/src/test/java/com/bitcoin/util/ObjectJsonSerializationTests.java
+++ bitcoin-excavator/excavator/src/test/java/com/bitcoin/util/ObjectJsonSerializationTests.java
@@ -30,6 +30,7 @@ public class ObjectJsonSerializationTests {
     }
 
     public ObjectJsonSerializationTests(BitcoinOptions options) {
+        options.setUrl(new String[]{"http://google.com"});
         this.options = options;
     }
 
@@ -37,6 +38,6 @@ public class ObjectJsonSerializationTests {
     public void isReversibleOperation() throws IOException {
         String serialized = serializer.serialize(options);
         BitcoinOptions deserialized = deserializer.deserialize(serialized);
-        assertThat(deserialized, is(equalTo(options)));
+        assertThat(deserialized.getUrl(), is(equalTo(options.getUrl())));
     }
 }
